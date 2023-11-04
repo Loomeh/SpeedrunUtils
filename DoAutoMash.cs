dcccd02493763cd0d411f950bcfa6300f7c25944
Ninja-Cookie <noreply>
2023-11-04 21:47:17 +0000

    Fix for AutoMash to more accurate skip text

--- DoAutoMash.cs
+++ DoAutoMash.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using System;
 using System.Threading.Tasks;
 using UnityEngine.UI;
+using UnityEngine.Playables;
+using TMPro;
 
 namespace SpeedrunUtils
 {
@@ -25,8 +27,11 @@ namespace SpeedrunUtils
         bool disabledExit;
         AudioManager audioManager;
         MethodInfo PlaySfxUI;
+        TextMeshProUGUI textLabel;
+        DialogueBehaviour currentDialogue;
+        bool fastForwardTypewriter;
 
-        public bool autoMash = true;
+        bool autoMash = true;
 
         private void Update()
         {
@@ -36,21 +41,26 @@ namespace SpeedrunUtils
                 if (player == null && worldHandler != null) { player = WorldHandler.instance.GetCurrentPlayer(); }
                 if (player != null) { sequenceState = (SequenceState)typeof(Player).GetField("sequenceState", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(player); }
                 if (seqHandler == null && worldHandler != null) { seqHandler = (SequenceHandler)typeof(WorldHandler).GetField("sequenceHandler", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(worldHandler); }
+                if (uiManager == null && seqHandler != null) { uiManager = (UIManager)typeof(SequenceHandler).GetField("uIManager", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(seqHandler); ; }
+                if (dialogueUI == null && uiManager != null) { dialogueUI = (DialogueUI)typeof(UIManager).GetField("dialogueUI", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(uiManager); }
+                if (dialogueUI != null) { textLabel = (TextMeshProUGUI)typeof(DialogueUI).GetField("textLabel", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(dialogueUI); }
+                if (dialogueUI != null) { currentDialogue = (DialogueBehaviour)typeof(DialogueUI).GetField("currentDialogue", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(dialogueUI); }
+                if (dialogueUI != null) { fastForwardTypewriter = (bool)typeof(DialogueUI).GetField("fastForwardTypewriter", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(dialogueUI); }
                 if (audioManager == null && seqHandler != null) { audioManager = (AudioManager)typeof(SequenceHandler).GetField("audioManager", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(seqHandler); }
                 if (PlaySfxUI == null && audioManager != null) { PlaySfxUI = typeof(AudioManager).GetMethod("PlaySfxUI", BindingFlags.NonPublic | BindingFlags.Instance, null, new Type[] { typeof(SfxCollectionID), typeof(AudioClipID), typeof(float) }, null); }
-                if (uiManager == null) { uiManager = Core.Instance.UIManager; }
-                if (dialogueUI == null && uiManager != null) { dialogueUI = (DialogueUI)typeof(UIManager).GetField("dialogueUI", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(uiManager); }
 
-                if (seqHandler != null && dialogueUI != null && audioManager != null && autoMash)
+                if (seqHandler != null && dialogueUI != null && audioManager != null && worldHandler != null && textLabel != null && autoMash)
                 {
-                    if (sequenceState == SequenceState.IN_SEQUENCE)
+                    SceneObjectsRegister sceneObjectsRegister = (SceneObjectsRegister)typeof(WorldHandler).GetField("sceneObjectsRegister", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(worldHandler);
+
+                    if (sequenceState == SequenceState.IN_SEQUENCE && sceneObjectsRegister != null)
                     {
                         FieldInfo skipTextActiveStateField = typeof(SequenceHandler).GetField("skipTextActiveState", BindingFlags.NonPublic | BindingFlags.Instance);
                         disabledExit = (bool)typeof(SequenceHandler).GetField("disabledExit", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public).GetValue(seqHandler);
                         bool flag = seqHandler.IsEnabled && !disabledExit && skipTextActiveStateField.GetValue(seqHandler).ToString() == "NOT_SKIPPABLE";
                         if (flag && dialogueUI.CanBeSkipped && !dialogueUI.isYesNoPromptEnabled)
                         {
-                            if (dialogueUI.ReadyToResume)
+                            if (currentDialogue != null && dialogueUI.ReadyToResume)
                             {
                                 seqHandler.ResumeSequence();
                                 if (dialogueUI.IsShowingDialogue() && PlaySfxUI != null)
@@ -59,7 +69,7 @@ namespace SpeedrunUtils
                                 }
                                 dialogueUI.EndDialogue();
                             }
-                            else
+                            else if (currentDialogue != null && textLabel.maxVisibleCharacters > 1 && textLabel.textInfo.characterCount != textLabel.maxVisibleCharacters && !fastForwardTypewriter)
                             {
                                 typeof(SequenceHandler).GetMethod("FastForwardTypewriter", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(seqHandler, new object[] { });
                             }
